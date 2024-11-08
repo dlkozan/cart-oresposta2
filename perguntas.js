650f94025bceb0d3b04f1dc62b4dec33716a1722
dlkozan <145031313+dlkozan@users.noreply.github.com>
2024-11-08 10:52:45 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Qual a maior ave do mundo?',
+    'Avestruz'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual o maior continente do mundo?',
+    'Asia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'conhecimento geral',
+    'Quantos ossos o corpo humano tem?',
+    '206'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Lingua portuguesa',
+    'Quantas letras há no alfabeto?',
+    '26'
 )
